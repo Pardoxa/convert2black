166e7e2e12971bacb225db949f858548f76ed24e
Yannick Feld <yannick.feld@uol.de>
2021-06-23 10:34:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Convert image to black and white
+
+Very small program used to preview a colored image in black and white.
+I use this, because a printed paper should also look good in black and white.
+Using this, I can preview how it looks.
---
+++ convert2black.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from PIL import Image, ImageEnhance
+import sys
+import pdf2image
+
+DPI = 200
+FORMAT = 'png'
+
+def pdf2pli(path):
+    img = pdf2image.convert_from_path(path, dpi=DPI, fmt=FORMAT)[0]
+    return img
+
+def get_img(path):
+    if path.endswith(".pdf"):
+        return pdf2pli(path)
+    else:
+        return Image.open(path)
+
+def main():
+    length = len(sys.argv)
+    mode = "L"
+    if length < 2:
+        print("Usage: ./convert2black pathToImageOrPdf <mode>")
+        print("mode is an optional parameter and can be for example L or 1 or filter")
+        exit(0)
+    elif length >= 3:
+        mode = sys.argv[2]
+    filename = sys.argv[1]
+
+    print(filename)
+    img = get_img(filename)
+    if mode == "filter":
+        filter = ImageEnhance.Color(img)
+        img = filter.enhance(0)
+    else:
+        img = img.convert(mode)
+    img.show()
+
+if __name__ == '__main__':
+    main()
