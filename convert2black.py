fd2e35be5da2c57fd0ade799ca5cf390ee16dddf
Yannick Feld <yannick.feld@uol.de>
2021-06-23 11:07:12 +0200

    convert whole pdf

--- convert2black.py
+++ convert2black.py
@@ -5,16 +5,17 @@ import pdf2image
 
 DPI = 200
 FORMAT = 'png'
+THREAD_COUNT = 8
 
 def pdf2pli(path):
-    img = pdf2image.convert_from_path(path, dpi=DPI, fmt=FORMAT)[0]
+    img = pdf2image.convert_from_path(path, dpi=DPI, fmt=FORMAT, thread_count=THREAD_COUNT)
     return img
 
 def get_img(path):
     if path.endswith(".pdf"):
         return pdf2pli(path)
     else:
-        return Image.open(path)
+        return [Image.open(path)]
 
 def main():
     length = len(sys.argv)
@@ -30,11 +31,15 @@ def main():
     print(filename)
     img = get_img(filename)
     if mode == "filter":
-        filter = ImageEnhance.Color(img)
-        img = filter.enhance(0)
+        for i in range(len(img)):
+            filter = ImageEnhance.Color(img[i])
+            img[i] = filter.enhance(0)
     else:
-        img = img.convert(mode)
-    img.show()
+        for i in range(len(img)):
+            img[i] = img[i].convert(mode)
+
+    for i in reversed(range(len(img))):
+        img[i].show()
 
 if __name__ == '__main__':
     main()
